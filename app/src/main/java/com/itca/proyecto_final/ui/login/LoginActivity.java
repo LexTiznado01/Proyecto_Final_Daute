9e46dfbdc64dc1d7357241fe637832d03d1c70dc
jorge1120 <jorge.lopez19@itca.edu.sv>
2021-11-11 11:35:41 -0600

    boton

--- app/src/main/java/com/itca/proyecto_final/ui/login/LoginActivity.java
+++ app/src/main/java/com/itca/proyecto_final/ui/login/LoginActivity.java
@@ -2,10 +2,12 @@ package com.itca.proyecto_final.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -14,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -23,6 +26,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.itca.proyecto_final.R;
 import com.itca.proyecto_final.ui.login.LoginViewModel;
 import com.itca.proyecto_final.ui.login.LoginViewModelFactory;
@@ -31,14 +39,22 @@ import com.itca.proyecto_final.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
 
 
-
-
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
+
+    private static final String TAG = "CustomAuthActivity";
+    private String mCustomToken;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -128,6 +144,50 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    // [END on_start_check_user]
+
+    private void startSignIn() {
+        // [START sign_in_custom]
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                    }
+                });
+        // [END sign_in_custom]
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+    public void onClick(View v) {
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
+
+    }
+
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
@@ -139,12 +199,4 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    public void onClick(View view) {
-        Button button = (Button) findViewById(R.id.app_bar_main);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-            }
-        });
-    }
 }
