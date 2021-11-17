aa788ae707c3ebf1d0f9e7cc25d238d12189d786
ByronPalacios21 <palaciosbyron582@gmail.com>
2021-11-17 15:43:06 -0600

    cambios

--- app/src/main/java/com/itca/proyecto_final/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/itca/proyecto_final/ui/gallery/GalleryFragment.java
@@ -1,9 +1,11 @@
 package com.itca.proyecto_final.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -27,6 +29,7 @@ public class GalleryFragment extends Fragment {
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
 
         final TextView textView = binding.textGallery;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
